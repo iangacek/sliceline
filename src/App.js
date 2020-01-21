527db7a20eac961e64b3d8ad87444851cf5010df
Ian Gacek <gacek004@umn.edu>
2020-01-21 11:45:41 -0600

    Refactored GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React from 'react';
-import { createGlobalStyle } from 'styled-components'
-import { Navbar} from "./Navbar/Navbar";
-import { Banner} from "./Banner/Banner";
-import { Menu} from "./Menu/Menu";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    font-family: 'Open Sans', sans-serif;
-  }
-
-  h1, h2, h3 {
-    font-family: 'Righteous', cursive;
-  }
-`
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import { Navbar } from "./Navbar/Navbar";
+import { Banner } from "./Banner/Banner";
+import { Menu } from "./Menu/Menu";
+import { GlobalStyle } from "./Styles/GlobalStyle";
 
 function App() {
   return (
     <>
-    <GlobalStyle/>
-    <Navbar/>
-    <Banner/>
-    <Menu/>
-    <div>Hello Sliceline</div>
+      <GlobalStyle />
+      <Navbar />
+      <Banner />
+      <Menu />
     </>
   );
 }
