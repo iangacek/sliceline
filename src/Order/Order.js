31f8c93631e0ede880e4098112c65cbb9a20e109
Ian Gacek <gacek004@umn.edu>
2020-01-21 13:14:12 -0600

    Add price
    Added price to menu labels and order list

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -5,6 +5,7 @@ import {
   DialogFooter,
   ConfirmButton
 } from "../FoodDialog/FoodDialog";
+import { formatPrice } from "../Data/FoodData";
 
 const OrderStyled = styled.div`
   position: fixed;
@@ -24,13 +25,38 @@ const OrderContent = styled(DialogContent)`
   height: 100%;
 `;
 
+const OrderContainer = styled.div`
+  padding: 10px 0px;
+  border-bottom: 1px solid grey;
+`;
+
+const OrderItem = styled.div`
+  padding: 10px 0px;
+  display: grid;
+  grid-template-columns: 20px 150px 20px 60px;
+  justify-content: space-between;
+`;
+
 export function Order({ orders }) {
   return (
     <OrderStyled>
       {orders.length === 0 ? (
         <OrderContent>Your order is currently empty.</OrderContent>
       ) : (
-        <OrderContent>Found {orders.length} orders</OrderContent>
+        <OrderContent>
+          {" "}
+          <OrderContainer> Your Order: </OrderContainer>{" "}
+          {orders.map(order => (
+            <OrderContainer>
+              <OrderItem>
+                <div>1</div>
+                <div>{order.name}</div>
+                <div/>
+                <div>{formatPrice(order.price)}</div>
+                </OrderItem>
+            </OrderContainer>
+          ))}
+        </OrderContent>
       )}
       <DialogFooter>
         <ConfirmButton>Checkout</ConfirmButton>
